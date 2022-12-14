c01d5f3e52651e004eacc4de6c8cc19ddae8c4fb
bbangjae <thebyunjae@naver.com>
2022-12-14 09:40:25 +0900

    bacup 삭제

--- backup/spring_boot_project_team/src/main/java/com/spring_boot/projectTeam/dao/IBookInfoDAO_bj.java
+++
@@ -1,7 +0,0 @@
-package com.spring_boot.projectTeam.dao;
-
-import com.spring_boot.projectTeam.model.BookInfoVO_b;
-
-public interface IBookInfoDAO_bj {
-	public BookInfoVO_b detailViewBook(String bNo);
-}
