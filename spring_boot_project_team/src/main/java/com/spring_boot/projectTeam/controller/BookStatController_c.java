37680c7529e061ca70b73ca44796106f5257d9eb
sn0wcub2 <chlbb174@naver.com>
2022-12-14 15:33:32 +0900

    마이페이지 작업 임시 백업  + 철현 이미지 추가

--- spring_boot_project_team/src/main/java/com/spring_boot/projectTeam/controller/BookStatController_c.java
+++ spring_boot_project_team/src/main/java/com/spring_boot/projectTeam/controller/BookStatController_c.java
@@ -1,6 +1,8 @@
 package com.spring_boot.projectTeam.controller;
 
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring_boot.projectTeam.model.BookInfoVO_b;
 import com.spring_boot.projectTeam.model.mybookVO;
 import com.spring_boot.projectTeam.service.BookStatService_c;
 
@@ -25,13 +28,17 @@ public class BookStatController_c {
 		 */
 		String memId ="hahaha";
 		
-		int bookList0 = service.statList0(memId);
+		int bookList0 = service.statList0(memId);  // 소유 중인 도서
 		model.addAttribute("bookList0", bookList0);
-		int bookList2 = service.statList2(memId);
+		int bookList2 = service.statList2(memId);  // 빌린 도서
 		model.addAttribute("bookList2", bookList2);
 		int bookList3 = service.statList3(memId);
-		model.addAttribute("bookList3", bookList3);
-		model.addAttribute("myName", memId);
+		model.addAttribute("bookList3", bookList3); // 관심 도서
+		model.addAttribute("myName", memId); // 아이디 출력
+		ArrayList<BookInfoVO_b> ownBookInfo = service.ownBookData(memId); // 소유 책 정보
+		model.addAttribute("ownBookInfo", ownBookInfo);
+		 
+		
 		return "mypage/mypageform";
 	}
 }
